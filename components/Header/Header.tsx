55062ae3f5ccdcb839b227c2bf0a31b59200bc14
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-01-18 11:24:21 +0200

    fix Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,19 @@
 import css from "./Header.module.css";
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className={css.header}>
-      <a href="/" aria-label="Home">
+      <Link href="/" aria-label="Home">
         NoteHub
-      </a>
+      </Link>
       <nav aria-label="Main Navigation">
         <ul className={css.navigation}>
           <li>
-            <a href="/">Home</a>
+            <Link href="/">Home</Link>
           </li>
           <li>
-            <a href="/notes">Notes</a>
+            <Link href="/notes">Notes</Link>
           </li>
         </ul>
       </nav>
