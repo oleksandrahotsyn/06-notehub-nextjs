ca3b9e34a6ae97e04f757b35c223cbe6d2723f71
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-01-18 11:43:02 +0200

    fix note.ts

--- types/note.ts
+++ types/note.ts
@@ -5,14 +5,6 @@ export interface Note {
   title: string;
   content: string;
   tag: NoteTag;
-  createdAt?: string;
-  created_at?: string;
-  created?: string;
-}
-export interface NotesResponse {
-  notes: Note[];
-  totalPages: number;
-  page?: number;
-  perPage?: number;
-  totalItems?: number;
+  createdAt: string;
+  updatedAt: string;
 }
